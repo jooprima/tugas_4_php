ab5bc39d90558eae257ef9c488f9f0c865a9f2ea
yohanes_prima <jooprima@gmail.com>
2019-10-07 10:47:21 +0700

    Tugas 4 php
    tugas ini dikumpulkan untuk niomic

---
+++ tugas4.php
@@ -0,0 +1,12 @@
+<?php
+$a=8000;
+$b=7500;
+$c=300;
+$d=($a-$b)/$c;
+
+echo "Harga beli = Rp. $a<br>
+      Harga jual = Rp. $b<br>
+      Keuntungan = Rp. $c<br>
+      Berapa jumlah kambing yang dibeli?<br>
+      Jawaban : $d ekor";
+ ?>
---
+++ tugas4_2.php
@@ -0,0 +1,10 @@
+<?php
+$x=523;
+$y=8891;
+$z=$y/$x;
+
+echo "Jumlah saluran = $x<br>
+      Jumlah terpakai = $y<br>
+      Rata-rata pemakaian = ...?<br>
+      Jawaban : $z liter/keluarga";
+ ?>
